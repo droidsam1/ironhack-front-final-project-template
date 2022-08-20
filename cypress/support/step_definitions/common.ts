52380f669165f5223a1ecb34d27a9a8cfe07679e
droidsam1 <droidsam@gmail.com>
2022-08-20 13:12:45 +0200

    broad log in expression in common steps

--- cypress/support/step_definitions/common.ts
+++ cypress/support/step_definitions/common.ts
@@ -2,7 +2,7 @@ import { Given } from "@badeball/cypress-cucumber-preprocessor";
 import { loginWithTestingUser } from "./e2e";
 import { interceptApiCall, waitForApiAResponse } from "./supabaseApi";
 
-Given("I am logged in to the app", async () => {
+Given("I am logged in( to the app)", async () => {
   interceptApiCall();
   loginWithTestingUser();
   waitForApiAResponse();
